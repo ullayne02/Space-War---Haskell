6a7c1e87e96c863817b641cd2e37eac38fc39f5b
Gabriel Melo <gvmgs@cin.ufpe.br>
2017-12-03 20:20:57 -0300

    include module Main, but has some bugs

---
+++ main.hs
@@ -0,0 +1,19 @@
+module Main where
+
+import Graphics.UI.Fungen
+import Screen
+--import Spacewar
+import Textures
+import Types
+
+gameCycle :: SpaceWarAction ()
+gameCycle = do
+    gs <- getGameState 
+
+main :: IO ()
+main = do
+    let winConfig = ((0,0), windowResolution, "Space War")
+        background = textureMap 0 (fst textureBackgroundSize) (snd textureBackgroundSize) 250 250
+        input = []
+        --gameCycle = (0 0 True) () (GameOver) ()
+    funInit winConfig background [] () () input gameCycle (Timer 40) bmpList
--- spacewar.hs
+++ spacewar.hs
@@ -1,14 +1,14 @@
-module Main where
+--module Main where
 
-import Graphics.UI.Fungen
-import Graphics.Rendering.OpenGL (GLdouble)
-import Data.Foldable
+--import Graphics.UI.Fungen
+--import Graphics.Rendering.OpenGL (GLdouble)
+--import Data.Foldable
 
-data GameAttribute = Score Int
-data GameState = Level Int
+--data GameAttribute = Score Int
+--data GameState = Level Int
 
-type SpaceWarObject = GameObject ()
-type SpaceWarAction a = IOGame GameAttribute () () () a
+--type SpaceWarObject = GameObject ()
+--type SpaceWarAction a = IOGame GameAttribute () () () a
 
-width = 480
-height = 640
+--width = 480
+--height = 640
